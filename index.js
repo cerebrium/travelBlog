dfc973d8deb51d8e0d314e6aa65fa5fad268826a
cerebrium <nicholas.m.shankland@gmail.com>
2020-01-09 14:51:53 -0800

    hurrah

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ db.on('error', (err) => {
 // mount all of my routes at their prefixes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/comment', require('./routes/comment'))
-app.get('*', function(req, res) {
-	res.sendFile(__dirname + '/client/public/index.html');
-});
+// app.get('*', function(req, res) {
+// 	res.sendFile(__dirname + '/client/public/index.html');
+// });
 
 // get the server listening on its port
 app.listen(process.env.PORT, () => {
